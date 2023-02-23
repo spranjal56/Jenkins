06aea6008c065ca98b85fb7fd48198588a39f931
spranjal56 <41280918+spranjal56@users.noreply.github.com>
2023-02-23 14:26:28 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo...24 Jan 2023...new \n")
 print("Adding new line to check")
-
+print("Modyfying the file to check working of Poll SCM")
 
 
 
