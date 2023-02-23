27b38eb0c03dca56fb8ce66c1e8617efcee18278
spranjal56 <41280918+spranjal56@users.noreply.github.com>
2023-02-23 14:23:02 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
 print("Hello World Demo...24 Jan 2023...new \n")
-ptint("Adding new line to check")
+print("Adding new line to check")
 
 
 
